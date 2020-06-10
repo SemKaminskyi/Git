c5d74b241fec87215cdb3cb6bf48bf7cbbf79681
SemKaminskyi <kaminskyisem@gmail.com>
2020-06-11 00:04:59 +0300

    one problem

--- app/src/main/java/com/gmail/kaminskysem/yourhelper/MainActivity.java
+++ app/src/main/java/com/gmail/kaminskysem/yourhelper/MainActivity.java
@@ -3,6 +3,11 @@ package com.gmail.kaminskysem.yourhelper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        EditText et = findViewById(R.id.et_test1);
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.d("Main Activity", " user entered text: "+s);
+                TextView tv = findViewById(R.id.et_test1);
+//                tv.setText(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                        }
+                        });
+                        }
+
+                        }
